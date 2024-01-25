2241101971831939c239ee889732fff513ffc379
amos.anurag <amos.anurag@rubicotech.in>
2024-01-25 07:16:35 +0530

    chngd post to get.

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.get('/myapp/user/:_id/editInfo', async (req, res) => {
             return res.status(404).json({ error: 'User not found' });
         }
 
-        res.status(200).json({ user });
+        res.json({ result : 'success', data : user });
     } catch (error) {
         console.error('Error fetching user data:', error);
         res.status(500).json({ error: 'Internal Server Error' });
